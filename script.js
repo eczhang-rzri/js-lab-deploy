2bd9acec1afde82a6a973980d19543fb160a2871
Clint Tuttle <clinttuttle@users.noreply.github.com>
2025-01-27 21:32:49 -0600

    commented out challenge 1b code from script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // alert ("Hello my name is <enter name here>");
 
 
-
-
 // //variables
 // const myName = ""
 // const fruits = ['', '', '', '', ''];
@@ -17,26 +15,26 @@ function sayHelloWorld() {
 
     // Step 2 - Update the alert to display a concatenation of the variables above that spell out something like this:
     // Hi my name is Clint.  I like apples and I drive a Silver Prius.  You'll need to add color to the car object
-    
+
 }
 
 //Challenge 1b
 function dogYears() {
     // Set a variable called age equal to your age
- 
+
     // divide the age variable by 7 and store it a variable called answer
-     
+
     // Challenge 1a - Display result in an alert
     alert ("In dog years you are: " + answer);
 
-    // Challenge 1b - Display result in an <h2> tag
-    let resultElement = document.getElementById("results1");
-    resultElement.textContent = "In dog years you are: " + answer;
+    //// Challenge 1b - Display result in an <h2> tag
+    // let resultElement = document.getElementById("results1");
+    // resultElement.textContent = "In dog years you are: " + answer;
   }
 
 
 
-//Challenge 2 
+//Challenge 2
  function Challenge2() {
 
  }
@@ -44,23 +42,23 @@ function dogYears() {
 
 
 
-//Challenge 3 
+//Challenge 3
  function Challenge3() {
-    
+
  }
 
 
 
- 
+
  //Challenge 4
  function Challenge4() {
- 
+
  }
 
 
  //Challenge 5
  function Challenge5() {
- 
+
 
  }
 
@@ -69,7 +67,7 @@ function dogYears() {
  function Challenge6() {
 
     let resultElement = document.getElementById("results6");
-    
+
     const message = "Seriously?  Just find something cool you can do in JavaScript and insert that into Challenge6 function and have it run when the 'Wow Me!' button is clicked.  Please remove this alert before you submit your work"
 
     resultElement.textContent = message;
